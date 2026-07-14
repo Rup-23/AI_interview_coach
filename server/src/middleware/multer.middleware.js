27356f77d04db0051e4e4254085121a3f262c058
Rup-23 <152003973+Rup-23@users.noreply.github.com>
2026-07-14 18:26:53 +0530

    upload fix

--- server/src/middleware/multer.middleware.js
+++ server/src/middleware/multer.middleware.js
@@ -1,12 +1,20 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+import path from "path";
+
+const uploadPath = path.join(process.cwd(), "public", "temp");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+  destination(req, file, cb) {
+    cb(null, uploadPath);
   },
 
-  filename: function (req, file, cb) {
+  filename(req, file, cb) {
     const uniqueName = `${Date.now()}${path.extname(file.originalname)}`;
     cb(null, uniqueName);
   },
